3a51c0511ad618ed8a7d8051068d00987031c35b
Anthony <anthony.matignon@gmail.com>
2026-01-28 11:55:47 +0100

    Ajout d'un avertissement avant le nettoyage des branches pour s'assurer que les modifications sont sauvegardées, avec une option pour continuer ou annuler l'opération.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/lipgloss/list"
@@ -10,6 +11,7 @@ import (
 var errorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF0000")).Bold(true).MarginTop(1)
 var successStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#05DF72")).MarginTop(1).Bold(true)
 var infoStyle = lipgloss.NewStyle().Underline(true).MarginTop(1)
+var warningStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFA500")).Bold(true).MarginTop(1)
 
 func main() {
 	var headStyle = lipgloss.NewStyle().
@@ -20,6 +22,20 @@ func main() {
 		Bold(true)
 
 	fmt.Println(headStyle.Render(" Git Branch Cleaner \n This tool only deletes branches that have been merged to main or master,\n or local branches without remote tracking."))
+
+	argsWithProg := os.Args
+	if len(argsWithProg) < 2 || argsWithProg[1] != "-f" {
+		fmt.Println(warningStyle.Render("Before starting, you should ensure that you have committed or stashed any changes,\n and that you have pushed any branches you want to keep to the remote repository.\n Are you sure you want to continue? (y/n)"))
+
+		var proceed string
+		fmt.Scanln(&proceed)
+		if proceed != "y" && proceed != "Y" {
+			fmt.Println(infoStyle.Render("Exiting Git Branch Cleaner. No changes were made."))
+			return
+		} else {
+			fmt.Println(infoStyle.Render("Proceeding with Git Branch Cleaner...\n Next time you can run with -f flag to skip this prompt."))
+		}
+	}
 
 	if !checkGitCommand() {
 		fmt.Println(errorStyle.Render("Error: Git is not installed or not found in PATH."))
